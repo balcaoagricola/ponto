9be56b1939a8e39e13df0cdac91fde9a251e032a
balcaoagricola <pontogasparnovo@gmail.com>
2026-09-05 12:50:55 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,7 +1,26 @@
-self.addEventListener('install', (e) => {
-  self.skipWaiting();
+const CACHE_NAME = 'balcao-agricola-ponto-v1';
+const urlsToCache = [
+  './',
+  './index.html'
+];
+
+// Instalação do Service Worker e salvaguarda em cache
+self.addEventListener('install', function(event) {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(function(cache) {
+      return cache.addAll(urlsToCache);
+    })
+  );
 });
 
-self.addEventListener('fetch', (e) => {
-  e.respondWith(fetch(e.request));
+// Interceção de pedidos para carregar offline
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    caches.match(event.request).then(function(response) {
+      return response || fetch(event.request);
+    }).catch(function() {
+      // Se estiver offline e tentar aceder à página principal, devolve a cache
+      return caches.match('./index.html');
+    })
+  );
 });
